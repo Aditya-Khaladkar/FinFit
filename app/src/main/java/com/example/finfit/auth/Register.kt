92d6ee39382fe809cf95e1c750a3af3c259ac89f
Aditya-Khaladkar <adityakhaladkar98@gmail.com>
2023-07-20 13:35:42 +0530

    changed ui for auth

---
+++ app/src/main/java/com/example/finfit/auth/Register.kt
@@ -0,0 +1,12 @@
+package com.example.finfit.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.finfit.R
+
+class Register : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
