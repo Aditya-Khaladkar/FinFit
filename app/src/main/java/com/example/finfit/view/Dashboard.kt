154a415c9d64b25808c225ca85338e3dbce16e68
Aditya-Khaladkar <adityakhaladkar98@gmail.com>
2023-07-21 09:50:28 +0530

    dashboard setup

---
+++ app/src/main/java/com/example/finfit/view/Dashboard.kt
@@ -0,0 +1,12 @@
+package com.example.finfit.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.finfit.R
+
+class Dashboard : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
