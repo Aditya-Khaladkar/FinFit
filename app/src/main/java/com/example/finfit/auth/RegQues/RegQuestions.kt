58bd9360047f3c9b6ace577d32eef3791b351204
Aditya-Khaladkar <adityakhaladkar98@gmail.com>
2023-07-22 19:53:05 +0530

    reg ques

---
+++ app/src/main/java/com/example/finfit/auth/RegQues/RegQuestions.kt
@@ -0,0 +1,12 @@
+package com.example.finfit.auth.RegQues
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.finfit.R
+
+class RegQuestions : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reg_questions)
+    }
+}
